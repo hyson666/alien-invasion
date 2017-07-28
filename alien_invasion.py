081d792c23210f3bc7de98235fbb12c41dcc266c
hyson666 <601852941@qq.com>
2017-07-28 22:28:38 +0800

    加入了外星人的显示

--- alien_invasion.py
+++ alien_invasion.py
@@ -6,29 +6,35 @@ import pygame
 from pygame.sprite import Group
 from settings import Settings   #引入设置类
 from ship import Ship
+from alien import Alien
 import game_functions as gf
 
 def run_game():
-    #初始化游戏并创建一个窗口，这里的参数主要用于绘制屏幕参数
+    # 初始化游戏并创建一个窗口，这里的参数主要用于绘制屏幕参数
     pygame.init()
     ui_settings = Settings()
     screen = pygame.display.set_mode((ui_settings.screen_width,ui_settings.screen_height))    #参数用列表传输屏幕大小宽、高
     pygame.display.set_caption("Alien Invasion")
+    alien = Alien(ui_settings, screen)
 
-    #给飞船对象传输当前屏幕对象而创建一个实例
+    # 给飞船对象传输当前屏幕对象而创建一个实例
     ship = Ship(ui_settings,screen)
     # 创建一个用于存储子弹的编组
     bullets = Group()
+    aliens = Group()
+
+    # 创建外星人群
+    gf.create_fleet(ui_settings, screen, aliens)
 
     bg_color = (230,230,230)    #使用RGB参数绘制
 
 #开始游戏的主循环
     while True:
-        #监视键盘和鼠标事件
+        # 监视键盘和鼠标事件
         gf.check_events(ui_settings, screen, ship, bullets)
-        #根据按键调整坐标再画图
+        # 根据按键调整坐标再画图
         ship.updata()
         gf.updata_bullets(bullets)
-        gf.updata_screen(ui_settings, screen, ship, bullets)
+        gf.updata_screen(ui_settings, screen, ship, aliens, bullets)
 
 run_game()
--- game_functions.py
+++ game_functions.py
@@ -4,6 +4,7 @@ import sys
 
 import pygame
 from bullet import Bullet
+from alien import Alien
 
 def check_keydown_events(event, ui_settings, screen, ship, bullets):
     # 飞船坐标加减改为开关就可以持续移动了
@@ -14,6 +15,8 @@ def check_keydown_events(event, ui_settings, screen, ship, bullets):
     elif event.key == pygame.K_SPACE:
         if len(bullets) < ui_settings.bullets_allowed:
             fire_bullet(ui_settings, screen, ship, bullets)
+    elif event.key == pygame.K_q:
+        sys.exit()
 
 def check_keyup_events(event, ui_settings, screen, ship, bullet):
     if event.key == pygame.K_LEFT:
@@ -25,16 +28,14 @@ def check_events(ui_settings, screen, ship, bullets):
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
-
         # 先进入按下键的事件再判断哪个键
         elif event.type == pygame.KEYDOWN:  #有按键按下时
             check_keydown_events(event, ui_settings, screen, ship, bullets)
-
         # 先进入放开键的事件再判断哪个键
         elif event.type == pygame.KEYUP:
             check_keyup_events(event, ui_settings, screen, ship, bullets)
 
-def updata_screen(ui_settings, screen, ship, bullets):
+def updata_screen(ui_settings, screen, ship, aliens, bullets):
     """更新屏幕上的图像，并且切换到新屏幕"""
     # 每次输出显示前都进行绘制(染色函数）
     screen.fill(ui_settings.bg_color)
@@ -42,6 +43,7 @@ def updata_screen(ui_settings, screen, ship, bullets):
     for bullet in bullets.sprites():
         bullet.draw_bullet()
     ship.blitme()
+    aliens.draw(screen)
     # 让最近绘制的屏幕可见(屏幕处理完成后输出)
     pygame.display.flip()
 
@@ -57,3 +59,26 @@ def fire_bullet(ui_settings, screen, ship, bullets):
     if len(bullets) < ui_settings.bullets_allowed:
         new_bullet = Bullet(ui_settings, screen, ship)
         bullets.add(new_bullet)
+
+def create_fleet(ui_settings, screen, aliens):
+    "创建外星人群"
+    # 创建一个外星人，并计算一行可以容纳多少个外形人
+    # 外星人间距为外星人宽度
+    alien = Alien(ui_settings, screen)
+    alien_width = alien.rect.width
+    alien = Alien(ui_settings, screen)
+    number_alien_x = get_number_aliens_x(ui_settings, alien.rect.width)
+
+    #创建第一行外星人
+    for alien_number in range(number_alien_x):
+        # 创建一个外星人并加入当前行
+        alien = Alien(ui_settings, screen)
+        alien.x = alien_width + 2 * alien_width * alien_number
+        alien.rect.x = alien.x
+        aliens.add(alien)
+
+def get_number_aliens_x(ui_settings, alien_width):
+    "计算每行可容纳多少个外星人"
+    available_space_x = ui_settings.screen_width - 2 * alien_width
+    number_alien_x = int(available_space_x / (2 * alien_width))
+    return number_alien_x
